c1cfed6e0525be0716a52c041aa51dc1f9d3f095
seoyeoning <minalunpark@naver.com>
2025-11-18 22:21:48 +0900

    init

---
+++ src/test/java/cat/board/CatBoardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBoardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
